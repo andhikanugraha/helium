bffeeed49f4ee25969da183582a39b3c38297d55
Andhika Nugraha <andhika.nugraha@gmail.com>
2008-09-27 15:11:34 +0700

    Fixed the code that checks for static paths

--- helium/helium.php
+++ helium/helium.php
@@ -64,12 +64,11 @@ try {
 	else {
 		if (strlen($he->request) > 1) {
 			$boom = explode('/', $he->request);
-			$boom[] = '';
 
 			while (array_pop($boom) !== null) {
 				$dir = implode('/', $boom);
-				$dir = SITE_PATH . '/' . $dir;
-				if (file_exists($dir))
+				$dir = SITE_PATH . $dir;
+				if ($dir != SITE_PATH && file_exists($dir))
 					throw new HeliumException(HeliumException::file_not_found);
 			}
 		}
