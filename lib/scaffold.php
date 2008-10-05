6b4516a1f147b9863378cb6af40c43fac3f5c5c2
Andhika Nugraha <andhika.nugraha@gmail.com>
2008-10-05 12:18:30 +0700

    fixed scaffolding where there were no records

--- lib/scaffold.php
+++ lib/scaffold.php
@@ -26,6 +26,9 @@ class Helium_Scaffold extends Helium_Controller {
 		$this->fields = get_fields($this->prototype);
 		$this->field_types = get_field_types($this->prototype);
 
+		if (!$this->items)
+			$this->items = array();
+
 		if ($did = $this->params['deleted'])
 			$this->last_delete = $did;
 		if ($did = $this->params['added'])
@@ -61,6 +64,30 @@ class Helium_Scaffold extends Helium_Controller {
 			$this->has_and_belongs_to_many[$key] = $value;
 		}
 	}
+	
+// 	public function build() {
+// 		$table_name = Inflector::pluralize($this->prototype);
+// 		$class_name = Inflector::classify($this->prototype);
+// 
+// 		$properties = get_class_vars($class_name);
+// 		$relations = array('__has_one', '__belongs_to','__has_many',  '__has_and_belongs_to_many');
+// 		$types = array();
+// 		$defaults = 
+// 		foreach ($properties as $property) {
+// 			if (in_array($property->name, $relations))
+// 				continue;
+// 		}
+// 
+// 		$query = <<<EOF
+// CREATE TABLE `$table_name` (
+// `id` int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+// EOF;
+// 		$query .= <<<EOF
+// `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+// `updated_at` TIMESTAMP NOT NULL
+// )
+// EOF;
+// 	}
 
 	public function destroy() {
 		$id = intval($this->params['id']);
