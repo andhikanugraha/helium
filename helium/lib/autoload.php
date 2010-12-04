778ea3ca46cc8525932669baced556da7beecf94
Andhika Nugraha <andhika.nugraha@gmail.com>
2010-12-05 00:04:40 +0700

    fixed __autoload to utilize new Helium::factory

--- helium/lib/autoload.php
+++ helium/lib/autoload.php
@@ -2,15 +2,9 @@
 
 // TO BE REWRITTEN
 function __autoload($class_name) {
-	$maybe_a_controller = false;
-	$file_name = Inflector::underscore($class_name);
+	$model_name = Inflector::underscore($class_name);
 
-	$model = Helium::conf('models') . '/' . $file_name . '.php';
-	$code = $maybe_a_controller ? HeliumException::no_controller : HeliumException::no_model;
-	if (!file_exists($model))
-		throw new HeliumException($code, $class_name);
-	elseif (file_exists($model))
-		require_once($model);
+	Helium::load_app_file('model', $model_name);
 
 	return;
 }
