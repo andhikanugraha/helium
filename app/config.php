11ea64098f8534749ca8f131045b1dd1211d0cdd
Andhika Nugraha <andhika.nugraha@gmail.com>
2010-12-05 00:44:02 +0700

    reverted config file into default

--- app/config.php
+++ app/config.php
@@ -4,16 +4,13 @@
 
 class HeliumConfiguration extends HeliumDefaults {
 	// absolute URL to the site
-	// public $base_uri = 'http://localhost'; // optional, but required for canonical URIs.
+	// public $base_uri = 'http://localhost'; // only required if Helium is not installed at root directory
+
+	public $app_name = 'helium';
 
     // database info
-    public $db_user = 'garasi';
-    public $db_pass = '2010';
-    public $db_name = 'garasi';			// optional; defaults to $db_user
+    public $db_user = 'user';
+    public $db_pass = 'pass';
+    public $db_name = 'name';			// optional; defaults to $db_user
     public $db_host = 'localhost';		// optional; defaults to localhost
-
-	public $sync_remote_gateway = 'http://jg.thursdaypeople.com/sync/gateway';
 }
-
-Helium::$canonize = false;
-Helium::$app_id = 'jualangarasi';
